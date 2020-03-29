5f87c8786574902c70a2eac8dc51dce82307c82f
Jude Okagu <juud0010@gmail.com>
2020-03-29 17:31:53 +0200

    first

---
+++ script.js
@@ -0,0 +1,77 @@
+
+/////////////////////////  OBJECT DESTRUCTURING
+
+
+// const person = {
+//   firstName: 'Jude',
+//   lastName: 'Cole',
+//   hobbies: ['swimming',"coding","movies"],
+//   address: {
+    
+//     street: "12 hollister st",
+//     city: "houston",
+//     state: "NY",
+//     zip: 3302
+//   }
+
+// }
+
+// person.email='hdhhdhd@yh.com'
+
+// const {firstName,lastName, address:{city,zip},hobbies,email} = person
+
+// email
+
+
+
+// //////// 
+
+// const person = {
+//   firstName :"jude",
+//   lastName :"cole",
+//   hobbies:["coding","movies","surfing"],
+//   address :{
+//     street: "22 hollister st",
+//     city:"houston"
+//   }
+  
+
+// }
+
+// person
+
+
+// const names = ['jude','emily','val','emeka']  
+
+
+// // names.('kate')
+
+// names.unshift('jj')
+
+// names
+
+// ////////////////
+// //////////////////////////////////ARRAY HELPERS
+
+// .slice() cuts an array at the point indicated
+// .split  cuts an arrayt and returns new array
+// .splice()  can either add or remove to an existing array.
+
+//////////////LOOPS
+
+// for(i = 0; i < 11; i++){
+//   console.log(i)
+// }
+
+// /////// while loops
+
+// let i = 0 
+// while(i< 11){
+//   console.log(i)
+//   i++
+// }
+
+///////////////////// HIGH ORDER LOOPS
+
+
+console.log('here we go')
