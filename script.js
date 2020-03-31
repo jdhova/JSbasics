95d07cb5b86c0ef1d44ce04b4180941a037bc4aa
Jude Okagu <juud0010@gmail.com>
2020-03-31 22:25:53 +0200

    OOP

--- script.js
+++ script.js
@@ -74,4 +74,142 @@
 ///////////////////// HIGH ORDER LOOPS
 
 
-console.log('here we go')
+
+// const names =[
+
+//     {
+//         firstName:'Juud',
+//         lastName : "cole",
+//         age:      22,
+//         isActive: true
+//     },
+//     {
+//         firstName:'Emily',
+//         lastName : "Adams",
+//         age:      12,
+//         isActive: true
+//     },
+//     {
+//         firstName:'Emeka',
+//         lastName : "Daniels",
+//         age:      20,
+//         isActive: false
+//     }
+  
+// ]
+
+// names.forEach((name)=> console.log(name.age))
+
+///// TINERY OPERATOR with Switch
+
+//  const awake = 'coding';
+//  console.log( (!awake ) ? " sleep" :"keepcoding")
+
+// switch(awake)  {
+//     case 'red':
+//         console.log("this is red")
+//         break;
+//     case 'blue':
+//         console.log('this is blue')
+//         break;
+//     default:
+//         console.log('Jude is awake')
+//         break;
+// }
+     
+
+
+
+////// OBJECT ORIENTED PROGRAMING
+
+/// CONSTRUCTOR FUNCTION
+
+function Person(fitrstName, lastName, dob) {
+    this.fitrstName = fitrstName;
+    this.lastName = lastName;
+    this.dob = new Date(dob)
+
+    // this.getFullName = () =>  `${this.fitrstName} ${this.lastName}`
+    
+    
+
+};
+
+Person.prototype.getFullName = () =>  `${this.fitrstName} ${this.lastName}`
+
+const Person1 = new Person('dave','cole', '3-09-1980');
+
+// console.log(Person1.getFullName());
+
+console.log(Person1.getFullName());
+
+
+
+
+
+/////////// FUNCTIONS
+
+// function addNum(num1, num2) {
+//     console.log(num1+num2)
+// }
+//  addNum( 8,3)
+
+
+
+//  const addNum = (num1, num2) =>  num1+num2
+ 
+//  console.log(addNum(3,5))
+
+
+
+
+// const x = 10;
+
+// const color = x< 10 ? "blue" : "red"
+
+// console.log(color)
+
+
+
+// names.forEach(function(name){
+//     console.log(name.firstName)
+// })
+
+////  HIGH ORDER ARRAY HELPERS 
+
+// const nameTxt = names.filter(function(name){
+//     return name.isActive === true
+// }).map(function(name){
+//     return name.firstName
+// })
+
+// console.log(nameTxt)
+
+
+
+// for(let name of names){
+
+// console.log(name.lastName)
+// }
+
+
+// names.forEach(function(name){
+//     console.log(name.age)
+// })
+
+// const nameTxt = names.map(function(name){
+//     return name.firstName
+// }).map(function(name){
+//     return name[1]
+// })
+// console.log(nameTxt) 
+
+
+
+// const actTxt = names.filter(function(name){
+//    return  name.isActive != true
+// }).map(function(name){
+//     return name.firstName
+// })
+
+// console.log(actTxt)
